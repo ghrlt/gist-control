3ec50cbcde138b0bf8cd53f2cba87cbc689e8442
Gaëtan <gaetan.hrlt@gmail.com>
2022-05-31 15:22:57 +0200

    Edited package structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gist Control
+--
+
+Use this module to create, view, edit, delete or list your gists!
+
+## Installation
+...
+
+## Usage
+...
+
+
--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 import setuptools
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
 setuptools.setup(
-	name='gist-control',
-	version='0.1',
-	scripts=['gist.py'],
-	author="Gaëtan Hérault",
-	author_email="gaetan.hrlt+dev@gmail.com",
-	description="Use this package to automate your Gists!",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/ghrlt/gist-control",
-	packages=setuptools.find_packages(),
-
-	install_requires=[
-		"requests"
-	],
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	],
- )
+    name="gist_control",
+    version="0.1",
+    author="Gaëtan Hérault",
+    author_email="gaetan.hrlt+dev@gmail.com",
+    description="Control your Github Gists!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ghrlt/gist-control",
+    project_urls={
+        "Bug Tracker": "https://github.com/ghrlt/gist-control/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "gist_control"},
+    packages=setuptools.find_packages(where="gist_control"),
+    python_requires=">=3.6",
+)
